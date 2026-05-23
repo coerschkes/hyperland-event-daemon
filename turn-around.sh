3ba6df606151e3f8b11554de5cec4023fc57cc76
Christian Oerschkes <christian.oerschkes@hotmail.de>
2026-05-23 21:02:32 +0200

    chore: Decouple the build from the system structure.

--- turn-around.sh
+++ turn-around.sh
@@ -2,4 +2,6 @@
 
 go generate ./...
 go build .
+systemctl --user stop hyprland-event-daemon.service
+sudo mv ./hyprland-event-daemon /usr/local/bin
 systemctl --user restart hyprland-event-daemon.service
